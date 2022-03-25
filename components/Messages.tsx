0f3db8c4c4a652980690984a65ee726c5ba3dfca
Iván Mermelstein <ivan.mermelstein@gmail.com>
2022-03-25 15:07:16 -0300

    Finished

--- components/Messages.tsx
+++ components/Messages.tsx
@@ -1,9 +1,28 @@
-import { FC } from 'react';
-import { ByMoralis, useMoralis } from 'react-moralis';
+import { FC, useRef } from 'react';
+import { ByMoralis, useMoralis, useMoralisQuery } from 'react-moralis';
+import Message from './Message';
 import SendMessage from './SendMessage';
+
+const MINS_DURATION = 3600;
 
 const Messages: FC = () => {
     const { user } = useMoralis();
+    const endOfMessagesRef = useRef(null);
+    const { data, error, isLoading } = useMoralisQuery(
+        'Messages',
+        query =>
+            query
+                .ascending('createdAt')
+                .greaterThan(
+                    'createdAt',
+                    new Date(Date.now() - 1000 * 60 * MINS_DURATION)
+                ),
+        [],
+        {
+            live: true
+        }
+    );
+
     return (
         <div className='pb-56'>
             <div className='my-5'>
@@ -12,11 +31,17 @@ const Messages: FC = () => {
                     variant='dark'
                 />
             </div>
-            <div></div>
-            <div className='flex justify-center'>
-                <SendMessage />
+            <div className='space-y-10 p-4'>
+                {data.map(message => (
+                    <Message key={message.id} message={message} />
+                ))}
             </div>
-            <div className='text-center text-gray-200 mt-5'>
+            <div className='flex justify-center'>
+                <SendMessage endOfMessagesRef={endOfMessagesRef} />
+            </div>
+            <div
+                ref={endOfMessagesRef}
+                className='text-center text-gray-200 mt-5'>
                 <p>{`You're up to date ${user.getUsername()}`} 🥑</p>
             </div>
         </div>
--- components/SendMessage.tsx
+++ components/SendMessage.tsx
@@ -1,16 +1,52 @@
-import { FC } from 'react';
+import { FC, MouseEvent, MutableRefObject, useState } from 'react';
 import { useMoralis } from 'react-moralis';
 
-const SendMessage: FC = () => {
-    const { user } = useMoralis();
+interface SendMessageProps {
+    endOfMessagesRef: MutableRefObject<any>;
+}
+
+const SendMessage: FC<SendMessageProps> = ({ endOfMessagesRef }) => {
+    const { user, Moralis } = useMoralis();
+    const [message, setMessage] = useState<string>('');
+
+    const sendMessage = (e: MouseEvent<HTMLElement>) => {
+        e.preventDefault();
+
+        if (!message) return;
+
+        const Messages = Moralis.Object.extend('Messages');
+        const messages = new Messages();
+
+        messages
+            .save({
+                message: message,
+                username: user.getUsername(),
+                ethAddress: user.get('ethAddress')
+            })
+            .then(message => {})
+            .catch(error => {
+                console.log(error.message);
+            });
+
+        endOfMessagesRef.current.scrollIntoView({ behavior: 'smooth' });
+        setMessage('');
+    };
+
     return (
         <form className='flex fixed bottom-10 w-10/12 bg-gray-900 px-6 py-4 max-w-2xl shadow-xl rounded-full border-2 border-blue-200'>
             <input
                 type='text'
                 className='outline-none bg-transparent text-gray-50 placeholder-gray-400 flex-grow pr-5'
                 placeholder={`Enter your message ${user.getUsername()}`}
+                value={message}
+                onChange={e => setMessage(e.target.value)}
             />
-            <button className='font-bold text-pink-500'>Send</button>
+            <button
+                onClick={sendMessage}
+                type='submit'
+                className='font-bold text-pink-500'>
+                Send
+            </button>
         </form>
     );
 };
